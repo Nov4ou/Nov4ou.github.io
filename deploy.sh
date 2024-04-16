35de86c9a67a72a0064b5bc22dd96be680c82eab
Nov4ou <nov4ou@outlook.com>
2024-04-16 19:56:40 +0800

    Updated blog content

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the blog root directory
+cd _site
+
+# Add all changed files to the staging area
+git add --all
+
+# Commit the changes with a short commit message
+git commit -m "Updated blog content"
+
+# Push the local repository to the remote repository
+git push -u origin main
+
+# Print a success message
+echo "Blog deployed successfully!"
